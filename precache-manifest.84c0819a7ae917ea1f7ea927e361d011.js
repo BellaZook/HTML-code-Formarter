57dc22deb578c60f9190166a299487191d3a7981
Anthony Robin <robinjly@hotmail.com>
2019-01-08 15:23:44 -0800

    Updates

---
+++ precache-manifest.84c0819a7ae917ea1f7ea927e361d011.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eabda7cddfe0f711f15c",
+    "url": "/HTML-code-Formartter/static/css/main.b3baa265.chunk.css"
+  },
+  {
+    "revision": "eabda7cddfe0f711f15c",
+    "url": "/HTML-code-Formartter/static/js/main.eabda7cd.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/HTML-code-Formartter/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "dbeba12d1fa12e7073b1",
+    "url": "/HTML-code-Formartter/static/js/runtime~main.dbeba12d.js"
+  },
+  {
+    "revision": "f46c68395483ca98d6178600ab99102b",
+    "url": "/HTML-code-Formartter/index.html"
+  }
+];
