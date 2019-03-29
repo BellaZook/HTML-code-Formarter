728c31f6258d039601fda771ce486b3a12d2650a
BellaZook <robinjly@gmail.com>
2019-03-29 09:30:08 -0700

    update text

--- src/components/tabs/Format/index.js
+++ src/components/tabs/Format/index.js
@@ -5,7 +5,7 @@ import MapColorButtons from './MapColorButtons';
 const Format = ({ radioFormat, handleRadioChange, handleAddColor, activeTab }) => {
     return (
         <div id="menu" className="tabContent" style={{ display: activeTab === 'Format' ? "block" : "none" }}>
-            <p style={{ margin: 0 }}>Select some text, then click to change the text color</p>
+            <p style={{ margin: 0 }}>Paste your code, then click on the convert button.</p>
             <div className="flexContainer flex-vertical flex-warp">
                 <RadioButtons
                     radioFormat={radioFormat}
